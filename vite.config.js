f55d5787a6d350abaa324d65bd894658723b5e8c
cem sahinkaya <mightyforever74@gmail.com>
2025-05-14 08:55:12 +0300

    Fix base path for GitHub Pages + HashRouter

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ZebercedSolarOptimizer360/', // GitHub Pages için zorunlu
+  base: './', // GitHub Pages için zorunlu
   plugins: [react()],
 })
